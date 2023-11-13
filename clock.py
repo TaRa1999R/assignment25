4fd8775c1de2fe84da1fa3008fa88a1f7507a540
TaRa1999R <rezaietara78@gmail.com>
2023-11-13 22:58:27 +0330

    assignment25

---
+++ clock.py
@@ -0,0 +1,14 @@
+
+import time
+
+class Time () :
+    def __init__ ( self , hour , min , sec ) :
+        self.hour = hour
+        self.minute = min
+        self.second = sec
+
+    def plus ( self ) :
+        ...
+
+    def minus ( self ) :
+        ...
