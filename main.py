c712e5280cd1bd3cab8e524bfdb282291df236c0
TaRa1999R <rezaietara78@gmail.com>
2023-11-13 23:14:15 +0330

    assignment25

---
+++ main.py
@@ -0,0 +1,12 @@
+
+import sys
+import time
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import Signal
+from world_clock import Wold_clock
+from alarm import Alarm
+from stop_watch import Stop_watch
+from timer_section import Timer
+
+
