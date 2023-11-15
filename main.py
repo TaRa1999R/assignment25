b3993dfa2b049fdcdf782b292893fec9b3b1d814
TaRa1999R <rezaietara78@gmail.com>
2023-11-16 00:02:01 +0330

    assignment25

--- main.py
+++ main.py
@@ -8,5 +8,20 @@ from world_clock import Wold_clock
 from alarm import Alarm
 from stop_watch import Stop_watch
 from timer_section import Timer
+from clock import Time
+from design_ui import Ui_MainWindow
 
+class Mainwindow ( QMainWindow ) :
+    
+    def __init__ ( self ) :
+        super().__init__()
+        self.ui = Ui_MainWindow ()
+        self.ui.setupUi (self)
+
+
+if __name__ == "__main__" :
+    app = QApplication (sys.argv)
+    window = Mainwindow ()
+    window.show ()
+    app.exec ()
 
